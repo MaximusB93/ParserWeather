1843ee0b3723f612b68d0e0b197b101ff747ffe0
MaximusB93 <maksim.budnikov.github@gmail.com>
2022-07-21 17:34:36 +0300

    Начало работы

---
+++ WeatherParserConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WeatherParserConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(@"https://world-weather.ru/pogoda/russia"); 
+
+                //HtmlNodeCollection collection = doc.GetElementbyId(“id”);
+
+        }
+    }
+}
