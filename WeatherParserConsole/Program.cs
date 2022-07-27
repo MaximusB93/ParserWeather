1438933899c3c18550fb4f3bbb07794f79cd072b
MaximusB93 <107109667+MaximusB93@users.noreply.github.com>
2022-07-27 07:36:22 +0300

    Update Program.cs

--- WeatherParserConsole/Program.cs
+++ WeatherParserConsole/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
-using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace WeatherParserConsole
 {
@@ -40,24 +40,83 @@ namespace WeatherParserConsole
             };
             HD = web.Load(html);
 
-            // Собственно, здесь и производится выборка интересующих нам нодов
-            // В данном случае выбираем блочные элементы с классом eTitle
             HtmlNodeCollection NoAltElements = HD.DocumentNode.SelectNodes("/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/ul/li/a");
 
-            // Проверяем наличие узлов
             if (NoAltElements != null)
-            {
-                foreach (HtmlNode HN in NoAltElements)
-                {
-                    // Получаем строчки
-                    string outputText = HN.InnerText;
-                    Console.WriteLine($"{outputText}\n");
-                }
-            
-            }
 
-         
+                for (int i = 0; i < NoAltElements.Count; i++)
+                {
+                    HtmlNode HN = NoAltElements[i];
+                    string outputText = HN.InnerText;
+                    Console.WriteLine($"{i}){outputText}\n");
+                }
+
+            Console.WriteLine("Выберите регион");
+            int IndexRegion = int.Parse(Console.ReadLine());
+
+            FuncGoroda();
 
         }
+
+        public static void FuncGoroda()
+        {
+            HtmlNodeCollection NoAltElements = null;
+            HN = NoAltElements[IndexRegion];
+
+            string Gorod = HN.Attributes["href"].Value;
+            HtmlDocument HD = new HtmlDocument();
+
+            var web = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8,
+            };
+            HD = web.Load(Gorod);
+
+            NoAltElements = HD.DocumentNode.SelectNodes("/html[1]/body[1]/div[1]/div[2]/div[1]/div[3]/div[1]/ul/li/a");
+
+            if (NoAltElements != null)
+
+                for (int i = 0; i < NoAltElements.Count; i++)
+                {
+                    HN = NoAltElements[i];
+                    string outputText = HN.InnerText;
+                    Console.WriteLine($"{i}){outputText}\n");
+                }
+        }
+        //string outputText = HN.Attributes["href"].Value;
+        //string html2 = NoAltElements[0].OuterHtml
+
+
+
+
+
+
+
+
     }
+
+
+
+
+
+    //private static object Parsing(string url)
+    //{
+    //    try
+    //    {
+    //        using (HttpClientHandler hd1 = new HttpClientHandler { })
+    //        {
+    //            using (var clien = new HttpClient(hd1))
+    //            {
+
+    //            }
+
+    //        }
+    //    }
+    //    catch (Exception ex) { Console.WriteLine(ex.Message); }
+    //    {
+
+    //        return null;
+    //    }
+    //}
 }
